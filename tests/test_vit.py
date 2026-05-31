a52acb957cb5be81faf246998c7e6f07b49ae723
ryo <ryo0208314@gmail.com>
2026-05-31 14:58:55 +0900

    feat: add test_vit.py

---
+++ tests/test_vit.py
@@ -0,0 +1,21 @@
+import torch
+
+from src.models.vision_transformer import VisionTransformer
+
+def test_vit():
+    model = VisionTransformer(
+        img_size=(224, 224),
+        patch_size=16,
+        in_channels=3,
+        d_model=768,
+        dropout=0.1,
+        num_layers=12,
+        num_heads=8,
+        qkv_bias=True,
+    )
+
+    x = torch.randn(2, 3, 224, 224)
+    y = model(x)
+
+    # num_tokens = num_patches + cls_token = 196 + 1 = 197
+    assert y.size() == (2, 197, 768)
