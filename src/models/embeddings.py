c4a158b15aa6e97bc8c22e9e8d6ba148934249c2
ryo <ryo0208314@gmail.com>
2026-05-31 12:31:44 +0900

    feat: implement LearnablePositionalEmbedding

--- src/models/embeddings.py
+++ src/models/embeddings.py
@@ -2,15 +2,12 @@ import math
 import torch
 import torch.nn as nn
 
-class PositionalEncoding(nn.Module):
-    """
-    Sinusoidal positional encoding.
-    """
-    def __init__(self, max_len: int, d_model: int) -> None:
+class SinusoidalPositionalEncoding(nn.Module):
+    def __init__(self, num_tokens: int, d_model: int) -> None:
         super().__init__()
 
-        pe = torch.zeros(max_len, d_model)
-        pos = torch.arange(0, max_len).unsqueeze(1) # (n, 1)
+        pe = torch.zeros(num_tokens, d_model)
+        pos = torch.arange(0, num_tokens).unsqueeze(1) # (n, 1)
         i = torch.arange(0, d_model, 2).unsqueeze(0) # (1, d_model / 2)
 
         div_term = torch.exp(
@@ -18,20 +15,32 @@ class PositionalEncoding(nn.Module):
         )
         pe[:, 0::2] = torch.sin(pos * div_term)
         pe[:, 1::2] = torch.cos(pos * div_term)
+        pe = pe.unsqueeze(0) # (1, num_tokens, d_model)
 
         self.register_buffer('pe', pe)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x + self.pe[:x.size(1), :]
+    def forward(self) -> torch.Tensor:
+        return self.pe
+
+class LearnablePositionalEmbedding(nn.Module):
+    def __init__(self, num_tokens: int, d_model: int) -> None:
+        super().__init__()
+
+        self.pe = nn.Parameter(
+            torch.randn(1, num_tokens, d_model)
+        )
+
+    def forward(self) -> torch.Tensor:
+        return self.pe
 
 class PatchEmbedding(nn.Module):
     """
     Patchifies and embeds.
     """
-    def __init__(self, channels: int, d_model: int, patch_size) -> None:
+    def __init__(self, in_channels: int, d_model: int, patch_size: int) -> None:
         super().__init__()
 
-        self.conv = nn.Conv2d(channels, d_model, kernel_size=patch_size, stride=patch_size)
+        self.conv = nn.Conv2d(in_channels, d_model, kernel_size=patch_size, stride=patch_size)
         self.flatten = nn.Flatten(2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
