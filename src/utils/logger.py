4377d1ef8af5f8e68d13d2f5d619814cce4be420
ryo <ryo0208314@gmail.com>
2026-06-03 14:12:00 +0900

    feat: implement get_logger

---
+++ src/utils/logger.py
@@ -0,0 +1,15 @@
+import sys
+import logging
+
+def get_logger(name: str = __name__) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.hasHandlers():
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s [%(levelname)s] [%(name)s]  %(message)s',
+            datefmt='%H:%M:%S'
+        )
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
